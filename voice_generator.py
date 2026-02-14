93f3558aa2e07ed7fb4764347c416ef5d5ecdee8
Sunandini Das <144526233+sunandini04@users.noreply.github.com>
2026-02-14 19:02:51 +0530

    made a change

--- voice_generator.py
+++ voice_generator.py
@@ -2,20 +2,27 @@ import edge_tts
 import asyncio
 import os
 
-OUTPUT_AUDIO_PATH = "output/voice.wav"
+def get_audio_path(topic):
+    safe_topic = topic.replace(" ", "_")
+    return f"output/{safe_topic}_voice.wav"
 
-async def generate_voice(script_text, output_path=OUTPUT_AUDIO_PATH):
+
+async def generate_voice(script_text, voice, output_path):
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
     communicate = edge_tts.Communicate(
         text=script_text,
-        voice="en-US-AriaNeural"
+        voice=voice
     )
-    await communicate.save(output_path)
-    return output_path  
 
-def text_to_speech(script_text):
+    await communicate.save(output_path)
+    return output_path
+
+
+def text_to_speech(script_text, voice,topic):
     """
-    Generates voice from text and returns the saved audio path.
+    Generates voice from text using selected language voice.
     """
-    path = asyncio.run(generate_voice(script_text))
+    output_path = get_audio_path(topic)
+    path = asyncio.run(generate_voice(script_text, voice,output_path))
     return path
